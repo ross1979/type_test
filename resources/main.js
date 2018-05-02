af2707b1111b25285efe1c590c659691f2e4e626
Ross Worby <30722772+ross1979@users.noreply.github.com>
2018-05-02 13:48:39 +0100

    initial load

---
+++ resources/main.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+  $('#text').on('keyup', (event) => {    			$('.preview').html($(event.currentTarget).val());
+  });
+
+  $('#font').on('change', event => {
+    $('.preview').css('font-family', $(event.currentTarget).val());
+  });
+
+
+   $('#weight').on('change', event => {
+    $('.preview').css('font-weight', $(event.currentTarget).val());
+  })
+
+
+  $('#size').on('keyup', event => {
+    var fontSize = $(event.currentTarget).val() + 'px';
+    $('.preview').css('font-size', fontSize);
+
+  })
+})
